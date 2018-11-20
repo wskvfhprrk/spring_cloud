1b6affe6bfbe26c32da2b594cf53e73dffa3412a
hejz <75412985@qq.com>
2018-11-20 16:54:29 +0800

    Refactored encryption algorithm

--- spring_boot_shiro/src/main/java/com/hejz/util/MD5Util.java
+++ spring_boot_shiro/src/main/java/com/hejz/util/MD5Util.java
@@ -3,16 +3,20 @@ package com.hejz.util;
 import org.apache.shiro.crypto.hash.SimpleHash;
 
 /**
+ * shiro密码重成器
  * @author 何建哲
  * @date 2018/11/20 16:34
  */
 public class MD5Util {
     public static void main(String[] args) {
+        String password = getPassword("admin", "admin");
+        System.out.println(password);
+    }
+
+    public static String getPassword(String username,String password) {
         String hashAlgorithmName = "MD5";
         int hashIterations = 1024;
-        String username = "admin";
-        String password = "admin";
         Object obj = new SimpleHash(hashAlgorithmName, username + password, null, hashIterations);
-        System.out.println(obj);
+        return String.valueOf(obj);
     }
 }
